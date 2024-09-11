89846cb623d731c317b29b6f4728c93fb91e9d1d
Sultan Ali <sultan.ali@devden.io>
2024-09-11 15:46:56 +0500

    fixing some linting errors

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -13,33 +13,33 @@ interface NavbarProps {
 const Navbar: React.FC<NavbarProps> = ({
   currentUser,
 }) => {
-  return ( 
+  return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div
         className="
-          py-4 
+          py-4
           border-b-[1px]
         "
       >
-      <Container>
-        <div 
-          className="
-            flex 
-            flex-row 
-            items-center 
+        <Container>
+          <div
+            className="
+            flex
+            flex-row
+            items-center
             justify-between
             gap-3
             md:gap-0
           "
-        >
-          <Logo />
-          <Search />
-          <UserMenu currentUser={currentUser} />
-        </div>
-      </Container>
+          >
+            <Logo />
+            <Search />
+            <UserMenu currentUser={currentUser} />
+          </div>
+        </Container>
+      </div>
+      <Categories />
     </div>
-    <Categories />
-  </div>
   );
 }
 
